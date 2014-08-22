7add0406a44b3dee10d81c5e90c0b7b1fe1d8ce9
Sterling Paramore <sterling.paramore@greatvines.com>
2014-08-22 10:34:23 -0700

    Can create a new job spec w/ template (choice of two templates)

--- app/models/job_spec.rb
+++ app/models/job_spec.rb
@@ -1,4 +1,7 @@
 class JobSpec < ActiveRecord::Base
+  JOB_TEMPLATE_TYPES = %w(TplBirstSoapGenericCommand TplBirstDuplicateSpace)
+  validates_inclusion_of :job_template_type, :in => JOB_TEMPLATE_TYPES, :allow_nil => false
+
   validates_uniqueness_of :name
   validates_presence_of :name
 
@@ -9,5 +12,13 @@ class JobSpec < ActiveRecord::Base
   
   def defaults
     self.enabled = false if self.enabled.nil?
+  end
+
+
+  def build_job_template(params)
+    raise "Unknown job_template_type: #{job_template_type}" unless JOB_TEMPLATE_TYPES.include?(job_template_type)
+
+    klass = job_template_type.constantize
+    self.job_template = klass.new(params.select { |param| klass::JOB_SPEC_PERMITTED_ATTRIBUTES.include?(param.to_sym) })
   end
 end
--- app/models/tpl_birst_duplicate_space.rb
+++ app/models/tpl_birst_duplicate_space.rb
@@ -1,4 +1,6 @@
 class TplBirstDuplicateSpace < ActiveRecord::Base
+  JOB_SPEC_PERMITTED_ATTRIBUTES = [:from_space_id_str, :to_space_name, :with_membership, :with_data, :with_datastore]
+
   validates_presence_of :from_space_id_str, :to_space_name
   has_one :job_spec, as: :job_template
 
