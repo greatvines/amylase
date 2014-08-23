b53d7a175362f6ef8b799745b15f67ea0c0f0c4a
Sterling Paramore <sterling.paramore@greatvines.com>
2014-08-22 17:54:32 -0700

    Model rspecs

--- app/models/tpl_birst_soap_generic_command.rb
+++ app/models/tpl_birst_soap_generic_command.rb
@@ -3,6 +3,4 @@ class TplBirstSoapGenericCommand < ActiveRecord::Base
 
   validates_presence_of :command
   has_one :job_spec, as: :job_template
-
-  accepts_nested_attributes_for :job_spec
 end
--- spec/models/job_spec_spec.rb
+++ spec/models/job_spec_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe JobSpec, :type => :model do
   before do
     @job_spec = JobSpec.new(
-      name:    "a_fascinating_job"
+      name:    "a_fascinating_job",
+      job_template_type: "TplBirstSoapGenericCommand",
+      job_template_attributes: {
+        command: "list_spaces"
+      }
     )
   end
 
@@ -11,15 +15,45 @@ RSpec.describe JobSpec, :type => :model do
 
   it { should respond_to(:name) }
   it { should respond_to(:enabled) }
+  it { should respond_to(:job_template) }
 
   it { should validate_uniqueness_of(:name) }
   it { should validate_presence_of(:name) }
+  it { should ensure_inclusion_of(:job_template_type).in_array(JobSpec::JOB_TEMPLATE_TYPES) }
 
   it { should belong_to(:job_template) }
   
   it { should be_valid }
 
+
+
   describe "default values" do
     specify { expect(@job_spec.enabled).to eq false }
+  end
+
+  describe "when name already exists" do
+    before do
+      job_spec_dup = @job_spec.dup
+      job_spec_dup.save
+      @job_spec.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "job_template associations" do
+    before { @job_spec.save }
+    let(:job_template_id) { @job_spec.job_template.id }
+
+    describe "existence of job_template" do
+      specify { expect(job_template_id).not_to be_nil }
+    end
+
+    describe "destroying a job_spec" do
+      specify {
+        expect { @job_spec.destroy }
+          .to change(@job_spec.job_template_type.constantize, :count).by(-1) 
+      }
+    end
   end
 end
