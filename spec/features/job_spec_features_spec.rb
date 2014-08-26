710a25783824a7f42bdee124723fdbeb6046518e
Sterling Paramore <sterling.paramore@greatvines.com>
2014-08-26 16:47:44 -0700

    JobSpec feature specs

---
+++ spec/features/job_spec_features_spec.rb
@@ -0,0 +1,36 @@
+# spec/features/job_spec_features_spec.rb
+
+require 'rails_helper'
+
+feature "User creates a JobSpec" do
+
+  before do
+    visit new_job_spec_path
+    fill_in 'Name', with: 'the_george_job'
+    check 'Enabled'
+    select 'TplBirstDuplicateSpace', from: 'Job template type'
+    fill_in 'From space id str', with: SecureRandom.uuid
+    fill_in 'To space name', with: 'george_II'
+    check 'With data'
+  end
+
+  context "with valid data" do
+    scenario "the creation was successful" do
+      click_button 'Create Job spec'
+
+      expect(page).to have_content 'Success! Job Spec created.'
+      expect(page).to have_content 'the_george_job'
+      expect(page).to have_content 'job_template_type: TplBirstDuplicateSpace'
+    end
+  end
+
+  context "with invalid data" do
+    scenario "the creation was unsuccessful" do
+      fill_in 'From space id str', with: 'not a valid UUI'
+      click_button 'Create Job spec'
+
+      expect(page).to have_content 'Error! Job Spec not created'
+      expect(page).to have_content 'space id must be a UUID'
+    end
+  end
+end
