074ebe08c5204df18874eec7751531d0b4520efd
Sterling Paramore <sterling.paramore@greatvines.com>
2014-10-06 14:26:42 -0700

    rails generate scaffold BirstSpace name:text client:references type:text space_id:string

---
+++ app/controllers/birst_spaces_controller.rb
@@ -0,0 +1,83 @@
+class BirstSpacesController < ApplicationController
+  before_action :set_birst_space, only: [:show, :edit, :update, :destroy]
+
+  # GET /birst_spaces
+  # GET /birst_spaces.json
+  def index
+    @birst_spaces = BirstSpace.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @birst_spaces }
+    end
+  end
+
+  # GET /birst_spaces/1
+  # GET /birst_spaces/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @birst_space }
+    end
+  end
+
+  # GET /birst_spaces/new
+  def new
+    @birst_space = BirstSpace.new
+  end
+
+  # GET /birst_spaces/1/edit
+  def edit
+  end
+
+  # POST /birst_spaces
+  # POST /birst_spaces.json
+  def create
+    @birst_space = BirstSpace.new(birst_space_params)
+
+    respond_to do |format|
+      if @birst_space.save
+        format.html { redirect_to @birst_space, notice: 'Birst space was successfully created.' }
+        format.json { render json: @birst_space, status: :created }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @birst_space.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /birst_spaces/1
+  # PATCH/PUT /birst_spaces/1.json
+  def update
+    respond_to do |format|
+      if @birst_space.update(birst_space_params)
+        format.html { redirect_to @birst_space, notice: 'Birst space was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @birst_space.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /birst_spaces/1
+  # DELETE /birst_spaces/1.json
+  def destroy
+    @birst_space.destroy
+    respond_to do |format|
+      format.html { redirect_to birst_spaces_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_birst_space
+      @birst_space = BirstSpace.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def birst_space_params
+      params.require(:birst_space).permit(:name, :client_id, :type, :space_id)
+    end
+end
---
+++ app/models/birst_space.rb
@@ -0,0 +1,3 @@
+class BirstSpace < ActiveRecord::Base
+  belongs_to :client
+end
