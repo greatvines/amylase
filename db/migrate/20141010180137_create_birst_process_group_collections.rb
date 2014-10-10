92db57543c6da81ddb2a5b427288fb49a1073694
Sterling Paramore <sterling.paramore@greatvines.com>
2014-10-10 11:20:21 -0700

    BirstProcessGroupCollection model and spec

--- db/migrate/20141010180137_create_birst_process_group_collections.rb
+++ db/migrate/20141010180137_create_birst_process_group_collections.rb
@@ -1,10 +1,12 @@
 class CreateBirstProcessGroupCollections < ActiveRecord::Migration
   def change
     create_table :birst_process_group_collections do |t|
-      t.text :name
+      t.text :name, :null => false
       t.text :description
 
       t.timestamps
     end
+
+    add_index :birst_process_group_collections, :name, unique: true
   end
 end
