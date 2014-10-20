3b33b412143170bf591ba930f81edf0c7dd9e602
Sterling Paramore <sterling.paramore@greatvines.com>
2014-10-20 16:52:28 -0700

    Server-side datatable processing
    I thought it was necessary to figure out server-side datatable views
    because the job list is going to increase very fast.
    
    Working copy, rebase me
    
    Rebase me - ajax datatable with colors and delayed search
    
    Some more progress
    
    Rebase - cleanup

--- app/helpers/launched_jobs_helper.rb
+++ app/helpers/launched_jobs_helper.rb
@@ -3,4 +3,21 @@ module LaunchedJobsHelper
     return '0s' unless seconds
     Rufus::Scheduler.to_duration(seconds.floor)
   end
+
+  def format_datetime(time)
+    time.strftime "%Y-%m-%d %H:%M:%S %Z"
+  end
+
+  def link_to_log(launched_job_id)
+    <<-EOF.unindent
+    <div class="hint">
+    #{
+      link_to job_log_path(launched_job_id), :target => '_blank', :title => 'Download Log', :data => { :toggle => 'tooltip' } do
+        raw '<span class="glyphicon glyphicon-download"></span>'
+      end
+    }
+    </div>
+    EOF
+  end
+
 end
